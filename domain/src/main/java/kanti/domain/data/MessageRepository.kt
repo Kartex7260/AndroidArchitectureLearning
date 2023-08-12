80d122ef6721a9aa2c234b0b0a80f63698911bfc
Kirill Zhitko <kirillzhitko@gmail.com>
2023-08-12 13:49:56 +0300

    Added MessageRepository.addNewMessage(Int, String)

--- domain/src/main/java/kanti/domain/data/MessageRepository.kt
+++ domain/src/main/java/kanti/domain/data/MessageRepository.kt
@@ -1,12 +1,27 @@
 package kanti.domain.data
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+
 class MessageRepository private constructor() : ArrayList<Message>() {
+
+    fun addNewMessage(userId: Int, text: String): Message {
+        val last = lastOrNull()
+        if (last == null) {
+            val message = Message(0, text, userId)
+            add(message)
+            return message
+        }
+        val message = Message(last.id + 1, text, userId)
+        return message
+    }
 
     companion object {
 
         private lateinit var _instance: MessageRepository
 
-        fun getInstance(): MessageRepository {
+        fun getInstance(dispatcher: CoroutineContext): MessageRepository {
             if (!::_instance.isInitialized)
                 _instance = MessageRepository().apply {
                     add(Message(0, "Хаааай", 0))
